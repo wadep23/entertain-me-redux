b84d2af41e57956cc8e848eaeb3626c2277be1d4
Bobby T <bob.tarino@gmail.com>
2021-08-14 11:17:46 -0500

    customized NavBar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -17,11 +17,11 @@ function AppNavbar() {
                             <Nav.Link href="#home">Home</Nav.Link>
                             <Nav.Link href="#link">Link</Nav.Link>
                             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                                <NavDropdown.Item href="#action/3.1">Sign SignUp</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">Login</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.1">Movies</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">Television</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">Video Games</NavDropdown.Item>
                                 <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.4">Vibe</NavDropdown.Item>
                             </NavDropdown>
                         </Nav>
                     </Navbar.Collapse>
