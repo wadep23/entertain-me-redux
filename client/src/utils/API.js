b3d82859f3dbbd9ec87c77d941e0ba205d7fd8a5
Wade Facer <wadepfacer@gmail.com>
2021-08-14 17:21:59 -0600

    modularized login, nav bar and sign up. Added modal component

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,17 +1,30 @@
-require('dotenv').config();
-
+require("dotenv").config();
 
 export const movieQuery = (genre) => {
-    return fetch("https://api.themoviedb.org/3/discover/movie?api_key="
-     + process.env.MOVIE_TV_API_KEY + "&language=en-US&with_genres=" + genre)
+  return fetch(
+    "https://api.themoviedb.org/3/discover/movie?api_key=" +
+      process.env.MOVIE_TV_API_KEY +
+      "&language=en-US&with_genres=" +
+      genre
+  );
 };
 
 export const tvQuery = (genre) => {
-    return fetch("https://api.themoviedb.org/3/discover/tv?api_key="
-    + process.env.MOVIE_TV_API_KEY + "&language=en-US&with_genres=" + genre)
+  return fetch(
+    "https://api.themoviedb.org/3/discover/tv?api_key=" +
+      process.env.MOVIE_TV_API_KEY +
+      "&language=en-US&with_genres=" +
+      genre
+  );
 };
 
 export const gameQuery = (genre, platform) => {
-    return fetch("https://api.rawg.io/api/games?key=" + process.env.GAME_API_KEY
-    + "&genres=" + genre + "&parent_platforms=" + platform);
+  return fetch(
+    "https://api.rawg.io/api/games?key=" +
+      process.env.GAME_API_KEY +
+      "&genres=" +
+      genre +
+      "&parent_platforms=" +
+      platform
+  );
 };
