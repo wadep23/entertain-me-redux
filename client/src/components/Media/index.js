c8750bb0cf3a9a29cea3bedf6227cf9b0d59e3cb
jakelw96 <jake.lw96@gmail.com>
2021-08-16 02:06:39 -0600

    More work on search

--- client/src/components/Media/index.js
+++ client/src/components/Media/index.js
@@ -1,19 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import {Button, Card, CardColumns, Container, Col } from 'react-bootstrap';
 import { useMutation } from '@apollo/client';
-import { useQuery } from '@apollo/client';
+import { useQuery, useLazyQuery } from '@apollo/client';
 import { SAVE_MOVIE } from '../../utils/mutations';
 import { MOVIE_API_QUERY }from '../../utils/queries';
 import Auth from '../../utils/auth';
 import { saveMediaIds, getSavedIds } from '../../utils/saveMedia';
-let genre = document.getElementById('movie-horror').value;
 
-const SearchMedia = () => {
+const SearchMedia = (props) => {
+    const { genreState, setGenreState } = props
     const [searchedMedia, setSearchedMedia] = useState([]);
     const [savedMedia, setSavedMedia] = useState(getSavedIds());
-    
     const { loading, data } = useQuery(MOVIE_API_QUERY, {
-        variables: { genre: genre }
+        variables: { genre: genreState }
     });
     const [saveMovie] = useMutation(SAVE_MOVIE);
 
@@ -25,26 +24,29 @@ const SearchMedia = () => {
         return <div>Loading...</div>
     }
 
-    const handleButtonClick = async (event) => {
-        event.preventDefault();
+    console.log(genreState)
+    
+
         
-        let imgLink = "https://image.tmdb.org/t/p/w500";
+            let imgLink = "https://image.tmdb.org/t/p/w500";
         
-        try {
-            let movieData = data.movie.map((movies) => ({
-               movieId: movies.id,
-               movieName: movies.original_title,
-               moviePoster: imgLink + movies.poster_path,
-               movieDetails: movies.overview,
-               movieRating: movies.vote_average
-            }));
+            try {
+                let movieData = data.movie.map((movies) => ({
+                    movieId: movies.id,
+                    movieName: movies.original_title,
+                    moviePoster: imgLink + movies.poster_path,
+                    movieDetails: movies.overview,
+                    movieRating: movies.vote_average
+                }));
 
             setSearchedMedia(movieData);
-            console.log(movieData)
-        } catch (err) {
-            console.error(err);
-        }
-    };
+            // setGenreState('');
+            } catch (err) {
+                console.error(err);
+            }
+       
+        
+    
 
     const handleSaveMedia = async (mediaId) => {
         const mediaToSave = searchedMedia.find((media) => media.id === media);
@@ -67,28 +69,28 @@ const SearchMedia = () => {
     };
 
     return (
-        <main class="return-data">
-            <button id="movie-horror" name="Movie" value="27" onClick={handleButtonClick}>Horror</button>
-            <button id="movie" name="Movie" value="27" onClick={handleButtonClick}>Horror</button>
+        <div class="return-data">
             <Container>
                 <CardColumns>
                     {searchedMedia.map((movies) => {
                         return (
-                            <Card key={movies.movieId} style={{ width: '18rem' }}>
-                                <Card.Img src={movies.moviePoster} alt={`The poster for ${movies.movieName}`} variant="top" />
-                                <Card.Body className="card-body">
-                                    <Card.Title>{movies.movieName}</Card.Title>
-                                    <Card.Text>{movies.movieDetails}</Card.Text>
-                                    <Card.Text>Rating: {movies.movieRating}</Card.Text>
-                                    <Button variant="primary">Save to your favorites!</Button>
-                                    <Button variant="primary">Watch a trailer!</Button>
-                                </Card.Body>
-                            </Card>
-                        )
+                            <Col sm={3}>
+                                <Card key={movies.movieId} style={{ width: '18rem' }}>
+                                    <Card.Img src={movies.moviePoster} alt={`The poster for ${movies.movieName}`} variant="top" />
+                                    <Card.Body className="card-body">
+                                        <Card.Title>{movies.movieName}</Card.Title>
+                                        <Card.Text>{movies.movieDetails}</Card.Text>
+                                        <Card.Text>Rating: {movies.movieRating}</Card.Text>
+                                        <Button variant="primary">Save to your favorites!</Button>
+                                        <Button variant="primary">Watch a trailer!</Button>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                        )   
                     })}
                 </CardColumns>
             </Container>
-        </main>
+        </div>
     );
 };
 
--- client/src/pages/Movie.js
+++ client/src/pages/Movie.js
@@ -1,27 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Redirect, useParams } from 'react-router-dom';
+import { Container, Col, Row } from 'react-bootstrap';
 import Auth from '../utils/auth';
+import SearchMedia from '../components/Media'
 
+const Movie = () => {
+  const [genreState, setGenreState] = useState('')
 
+  // const handleButtonClick = async (event) => {
+  //   event.preventDefault();
+  //   let genre;
 
-
-
-
-
-
-
-const Movie = props => {
+  //   if (document.getElementById())
+  // };
   
-
   return (
-    <div>
+    <main>
+      <div>
       <div className="flex-row mb-3">
         <h2 className="bg-dark text-secondary p-3 display-inline-block">
-          Viewing movie search.
+          So, you need a movie to watch? Click on a genre below and watch the magic happen!
         </h2>
-        <button className="btn ml-auto">
-          Add Favorite
-        </button>
+        <Container>
+          <Row>
+            <Col>
+              <button value="27" onClick={(j) => setGenreState(j.target.value)}>Horror</button>
+            </Col>
+            <Col>
+              <button value="28" onClick={(j) => setGenreState(j.target.value)}>Action</button>
+            </Col>
+            <Col>
+              <button value="12" onClick={(j) => setGenreState(j.target.value)}>Adventure</button>
+            </Col>
+            <Col>
+              <button value="10749" onClick={(j) => setGenreState(j.target.value)}>Romance</button>
+            </Col>
+            <Col>
+              <button value="878" onClick={(j) => setGenreState(j.target.value)}>Sci-Fi</button>
+            </Col>
+            <Col>
+              <button value="18" onClick={(j) => setGenreState(j.target.value)}>Drama</button>
+            </Col>
+            <Col>
+              <button value="14" onClick={(j) => setGenreState(j.target.value)}>Fantasy</button>
+            </Col>
+            <Col>
+              <button value="9648" onClick={(j) => setGenreState(j.target.value)}>Mystery</button>
+            </Col>
+            <Col>
+              <button value="35" onClick={(j) => setGenreState(j.target.value)}>Comedy</button>
+            </Col>
+          </Row>
+          <Row>
+            <SearchMedia
+              genreState={genreState}
+              setGenreState={setGenreState}
+            ></SearchMedia>
+          </Row>
+        </Container>
       </div>
 
       <div className="flex-row justify-space-between mb-3">
@@ -34,6 +70,8 @@ const Movie = props => {
       </div>
       <div></div>
     </div>
+    </main>
+    
 )};
 
 export default Movie;
