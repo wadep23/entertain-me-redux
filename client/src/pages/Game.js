83aa5cebb52c793f78a18c9978fb18d97563733b
Bobby T <bob.tarino@gmail.com>
2021-08-17 22:28:55 -0500

    imported react-icons for game query buttons

--- client/src/pages/Game.js
+++ client/src/pages/Game.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Redirect } from 'react-router';
 import auth from '../utils/auth';
 import SearchMedia from '../components/Game'
+import { IoGameController } from "react-icons/io5";
 
 const Game = () => {
   if (!auth.loggedIn()) {
@@ -12,13 +13,13 @@ const Game = () => {
     <main>
       <div>
         <div className="flex-row mb-3">
-          <h2 className="bg-dark text-secondary p-3 display-inline-block">
-            So, you need a game to play? Select your platform and click on a genre below to watch the magic happen!
+          <h2 className="bg-dark text-secondary p-3 display-inline-block text-center">
+            <IoGameController /> Select Platform & Game genre:
           </h2>
           <SearchMedia />
         </div>
       </div>
-    </main> 
-)};
+    </main>
+  )};
 
 export default Game;
