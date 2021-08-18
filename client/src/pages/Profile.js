eb38dbdb8b68cf1e80fd5cd756a8e5301becbbce
Chewie <chewie1985@gmail.com>
2021-08-18 04:05:29 -0600

    styled the profile page to match card layout. Included ability to watch trailers in cards

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react';
 import { Redirect, useParams } from "react-router-dom";
 import {
   ADD_FRIEND,
@@ -15,12 +16,21 @@ import {
   Row,
   Col,
 } from "react-bootstrap";
+import MovieModal from '../components/MovieModal';
+import TvModal from '../components/TvModal';
+import GameModal from '../components/GameModal';
 import FriendList from "../components/FriendsList";
 import Auth from "../utils/auth";
 
 const Profile = () => {
   const { username: userParam } = useParams();
-  
+
+  const [showMovieTrailerModal, setShowMovieTrailerModal] = useState(false);
+  const [movieTrailerModalTitle, setMovieTrailerModalTitle] = useState('');
+  const [showTvTrailerModal, setShowTvTrailerModal] = useState(false);
+  const [tvTrailerModalTitle, setTvTrailerModalTitle] = useState('');
+  const [showGameTrailerModal, setShowGameTrailerModal] = useState(false);
+  const [gameTrailerModalTitle, setGameTrailerModalTitle] = useState('');
 
   const [addFriend] = useMutation(ADD_FRIEND);
   const [deleteContent] = useMutation(
@@ -141,126 +151,148 @@ const Profile = () => {
   }
 
   return (
-    <>
-      <div fluid className="text-light bg-dark">
-        <Container>
-          <Row>
-            <Col md="4">
-              <h2>Welcome to {userParam ? `${userData.username}'s` : 'your'} Profile!</h2>
-            </Col>
-            <Col sm="auto">
-              {userParam && (
-                <button onClick={handleClick} size="lg">
-                  Add Friend
-                </button>
-              )}
-            </Col>
-          </Row>
-        </Container>
-      </div>
+    <main>
+      <div>
+        <div className="flex-row mb-3">
+          <h2 className="bg-dark text-secondary p-3 display-inline-block text-center">
+            Welcome to {userParam ? `${userData.username}'s` : 'your'} Profile!
+          </h2>
+        </div>
+        <div className="btn-loc">
+          {userParam && (
+            <button onClick={handleClick} size="lg">
+              Add Friend
+            </button>
+          )}
+        </div>
       <Container>
         <h2>
           {userData.favoriteMovies.length
             ? `Viewing ${userData.favoriteMovies.length} saved ${
-                userData.favoriteMovies.length === 1 ? "movie" : "movies"
+              userData.favoriteMovies.length === 1 ? "Movie" : "Movies"
               }:`
-            : "You have no saved Movies!"}
+              : "You have no saved Movies!"}
         </h2>
-        <CardColumns>
+        <Row fluid="true">
           {userData.favoriteMovies.map((user) => {
             return (
-              <Card key={user.movieId} border="dark">
-                {user.moviePoster ? (
-                  <Card.Img
-                    src={user.moviePoster}
-                    alt={`The cover for ${user.movieName}`}
-                    variant="top"
-                  />
-                ) : null}
-                <Card.Body>
-                  <Card.Title>{user.movieName}</Card.Title>
-                  <Card.Text>{user.movieDetails}</Card.Text>
-                  <Button
-                    className="btn-block btn-danger"
-                    onClick={() => handleDeleteContent(user.movieId)}
-                  >
-                    Delete this Movie!
-                  </Button>
-                </Card.Body>
-              </Card>
+              <Col sm={3}>
+                <Card key={user.movieId} style={{ width: '18rem' }}>
+                  <Card.Img src={user.moviePoster} alt={`The poster for ${user.movieName}`} variant="top" />
+                  <Card.Body className="card-body">
+                      <Card.Title>{user.movieName}</Card.Title>
+                      <Card.Text>{user.movieDetails}</Card.Text>
+                      <Card.Text>Rating: {user.movieRating}</Card.Text>
+                      {Auth.loggedIn() && (
+                        <Button className="btn-block btn-danger"
+                        onClick={() => handleDeleteContent(user.movieId)}
+                        >Delete this Movie!
+                        </Button>
+                      )}
+                        <Button variant="primary" 
+                          onClick={() => {setShowMovieTrailerModal(true)
+                            setMovieTrailerModalTitle(user.movieName + " Movie Trailer"); }}
+                          >Watch a trailer!
+                        </Button>
+                  </Card.Body>
+                </Card>
+              </Col>
             );
           })}
-        </CardColumns>
+        </Row>
       </Container>
+      <MovieModal
+        showMovieTrailerModal={showMovieTrailerModal}
+        setShowMovieTrailerModal={setShowMovieTrailerModal}
+        movieTrailerModalTitle={movieTrailerModalTitle}
+        setMovieTrailerModalTitle={setMovieTrailerModalTitle}
+        ></MovieModal>  
       <Container>
         <h2>
           {userData.favoriteTvShows.length
             ? `Viewing ${userData.favoriteTvShows.length} saved ${
-                userData.favoriteTvShows.length === 1 ? "tv show" : "tv shows"
+              userData.favoriteTvShows.length === 1 ? "T.V. show" : "T.V. shows"
               }:`
             : "You have no saved T.V. Shows!"}
         </h2>
-        <CardColumns>
+        <Row fluid="true">
           {userData.favoriteTvShows.map((user) => {
             return (
-              <Card key={user.tvShowId} border="dark">
-                {user.tvShowPoster ? (
-                  <Card.Img
-                    src={user.tvShowPoster}
-                    alt={`The cover for ${user.tvShowName}`}
-                    variant="top"
-                  />
-                ) : null}
-                <Card.Body>
-                  <Card.Title>{user.tvShowName}</Card.Title>
-                  <Card.Text>{user.tvShowDetails}</Card.Text>
-                  <Button
-                    className="btn-block btn-danger"
-                    onClick={() => handleDeleteContent(user.tvShowId)}
-                  >
-                    Delete this Tv Show!
-                  </Button>
-                </Card.Body>
-              </Card>
+              <Col sm={3}>
+                <Card key={user.tvShowId} style={{ width: '18rem' }}>
+                  <Card.Img src={user.tvShowPoster} alt={`The poster for ${user.tvShowName}`} variant="top" />
+                  <Card.Body className="card-body">
+                    <Card.Title>{user.tvShowName}</Card.Title>
+                    <Card.Text>{user.tvShowDetails}</Card.Text>
+                    <Card.Text>Rating: {user.tvShowRating}</Card.Text>
+                    {Auth.loggedIn() && (
+                      <Button className="btn-block btn-danger"
+                      onClick={() => handleDeleteContent(user.tvShowId)}
+                      >
+                        Delete this T.V. Show!
+                      </Button>
+                    )}
+                    <Button variant="primary" onClick={() => {setShowTvTrailerModal(true)
+                    setTvTrailerModalTitle(user.tvShowName + " Show Trailer"); }}
+                    >Watch a trailer!</Button>
+                  </Card.Body>
+                </Card>
+              </Col>
             );
           })}
-        </CardColumns>
+        </Row>
       </Container>
+      <TvModal
+        showTvTrailerModal={showTvTrailerModal}
+        setShowTvTrailerModal={setShowTvTrailerModal}
+        tvTrailerModalTitle={tvTrailerModalTitle}
+        setTvTrailerModalTitle={setTvTrailerModalTitle}
+        ></TvModal>
       <Container>
         <h2>
           {userData.favoriteGames.length
             ? `Viewing ${userData.favoriteGames.length} saved ${
-                userData.favoriteGames.length === 1 ? "game" : "games"
-              }:`
+              userData.favoriteGames.length === 1 ? "game" : "games"
+            }:`
             : "You have no saved Games!"}
         </h2>
-        <CardColumns>
+        <Row fluid="true">
           {userData.favoriteGames.map((user) => {
             return (
-              <Card key={user.gameId} border="dark">
-                {user.gamePoster ? (
-                  <Card.Img
-                    src={user.gamePoster}
-                    alt={`The cover for ${user.gameName}`}
-                    variant="top"
-                  />
-                ) : null}
-                <Card.Body>
+              <Col sm={3}>
+                <Card key={user.gameId} style={{ width: '18rem' }}>
+                  <Card.Img src={user.gamePoster} alt={`The poster for ${user.gameName}`} variant="top" />
+                  <Card.Body className="card-body">
                   <Card.Title>{user.gameName}</Card.Title>
-                  <Card.Text>{user.gameDetails}</Card.Text>
-                  <Button
-                    className="btn-block btn-danger"
-                    onClick={() => handleDeleteContent(user.gameId)}
-                  >
-                    Delete this Game!
-                  </Button>
-                </Card.Body>
-              </Card>
+                    <Card.Text>Rating: {user.gameRating}</Card.Text>
+                    {Auth.loggedIn() && (
+                      <Button className="btn-block btn-danger"
+                      onClick={() => handleDeleteContent(user.tvShowId)}
+                      >
+                      Delete this Game!
+                    </Button>
+                    )}
+                    <Button variant="primary" 
+                    onClick={() => {setShowGameTrailerModal(true)
+                    setGameTrailerModalTitle(user.gameName + " game Trailer"); }}
+                    >
+                      Watch a trailer!
+                    </Button>
+                  </Card.Body>
+                </Card>
+              </Col>   
             );
           })}
-        </CardColumns>
+        </Row>
       </Container>
-    </>
+      <GameModal
+        showGameTrailerModal={showGameTrailerModal}
+        setShowGameTrailerModal={setShowGameTrailerModal}
+        gameTrailerModalTitle={gameTrailerModalTitle}
+        setGameTrailerModalTitle={setGameTrailerModalTitle}
+      ></GameModal> 
+      </div>
+    </main>
   );
 };
 
